f194a2f0b2fa54a56978544361fbbdb8f95f597f
AneuryEstevez <87092561+AneuryEstevez@users.noreply.github.com>
2024-03-18 22:44:52 -0400

    updates

--- src/App.vue
+++ src/App.vue
@@ -34,8 +34,11 @@
             <td>{{reserva.id}}</td>
             <td>{{reserva.nombre}}</td>
             <td>{{reserva.laboratorio}}</td>
-            <td>{{reserva.fecha}}</td>
-
+            <td>{{reserva.fechaReserva}}</td>
+            <td>
+              <button class="btn btn-primary me-3" data-bs-toggle="modal" data-bs-target="#editModal" @click="openEditModal(reserva)">Editar</button>
+              <button class="btn btn-danger" @click="removeReserva(reserva.idReserva)">Eliminar</button>
+            </td>
           </tr>
           </tbody>
         <tbody v-else>
@@ -44,10 +47,10 @@
             <td>{{reserva.id}}</td>
             <td>{{reserva.nombre}}</td>
             <td>{{reserva.laboratorio}}</td>
-            <td>{{reserva.fecha}}</td>
+            <td>{{ formatDate(reserva.fechaReserva) }}</td>
             <td>
-              <button class="btn btn-primary me-3" @click="openEditModal(reserva)">Editar</button>
-              <button class="btn btn-danger" @click="removeReserva(reserva.id)">Eliminar</button>
+              <button class="btn btn-primary me-3" data-bs-toggle="modal" data-bs-target="#editModal" @click="openEditModal(reserva)">Editar</button>
+              <button class="btn btn-danger" @click="removeReserva(reserva.idReserva)">Eliminar</button>
             </td>
           </tr>
         </tbody>
@@ -82,7 +85,7 @@
               </div>
               <div class="mb-3">
                 <label for="fecha" class="form-label">Fecha</label>
-                <input type="datetime-local" class="form-control" id="fecha" v-model="fecha" :min="getCurrentDateTime()" required>
+                <input type="datetime-local" class="form-control" id="fecha" v-model="fechaReserva" :min="getCurrentDateTime()" required>
               </div>
             </form>
           </div>
@@ -93,109 +96,189 @@
         </div>
       </div>
     </div>
+
+    <!--Editar Modal-->
+    <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="exampleModalLabel">Editar Reserva</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <form id="editReserva" @submit.prevent="editReserva">
+              <div class="mb-3">
+                <label for="id" class="form-label">ID</label>
+                <input type="text" class="form-control" id="id" v-model="id" required>
+              </div>
+              <div class="mb-3">
+                <label for="nombre" class="form-label">Nombre</label>
+                <input type="text" class="form-control" id="nombre" v-model="nombre" required>
+              </div>
+              <div class="mb-3">
+                <label for="correo" class="form-label">Email</label>
+                <input type="email" class="form-control" id="correo" v-model="correo" required>
+              </div>
+              <div class="mb-3">
+                <label for="laboratorio" class="form-label">Laboratorio</label>
+                <input type="text" class="form-control" id="laboratorio" v-model="laboratorio" required>
+              </div>
+              <div class="mb-3">
+                <label for="fecha" class="form-label">Fecha</label>
+                <input type="datetime-local" class="form-control" id="fecha" v-model="fechaReserva" :min="getCurrentDateTime()" required>
+              </div>
+            </form>
+          </div>
+          <div class="modal-footer">
+            <button type="button" ref="closeEdit" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+            <button type="submit" form="editReserva" class="btn btn-primary">Editar</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 
 </template>
 
 <script>
+
+  const apiUrl = 'https://gltewsmc6d.execute-api.us-east-1.amazonaws.com/default/practica-7';
   export default {
 
     data() {
       return {
         reservas: [],
+        idReserva: "",
         id: "",
         nombre: "",
         laboratorio: "",
-        fecha: this.getCurrentDateTime(),
+        fechaReserva: this.getCurrentDateTime(),
         email: "",
         correo: "",
         mostrarPasadas: false
       }
     },
     async created() {
-      let response = await fetch('https://0x2l3y6r90.execute-api.us-east-1.amazonaws.com/default/myFuncion')
-      response = await response.json();
-      this.reservas = response.reservas;
-
-      console.log(this.reservas)
-
+      await this.fetchReservas();
     },
     computed: {
       reservasActivas() {
         const ahora = new Date();
-        return this.reservas.filter(reserva => new Date(reserva.fecha) > ahora);
+        return this.reservas.filter(reserva => new Date(reserva.fechaReserva) > ahora);
       },
       reservasPasadas() {
         const ahora = new Date();
-        return this.reservas.filter(reserva => new Date(reserva.fecha) <= ahora);
+        return this.reservas.filter(reserva => new Date(reserva.fechaReserva) <= ahora);
       }
     },
     methods: {
-      addReserva() {
-
-        // Find the reservation with the matching ID
-        const index = this.reservas.findIndex(reserva => reserva.id === this.id);
-        let reserva;
-
-        if (index > -1) {
-          reserva = this.reservas.splice(index, 1, {
+      async fetchReservas() {
+        try {
+          const response = await fetch(apiUrl);
+          const jsonResponse = await response.json();
+          this.reservas = jsonResponse.reservas;
+        } catch (error) {
+          console.error('Error fetching reservations:', error);
+        }
+      },
+      async addReserva() {
+        let reserva = {
             id: this.id,
             nombre: this.nombre,
             correo: this.correo,
             laboratorio: this.laboratorio,
-            fecha: this.fecha
-          });
-        } else {
-          reserva = {
-            id: this.id,
-            nombre: this.nombre,
-            correo: this.correo,
-            laboratorio: this.laboratorio,
-            fecha: this.fecha
-          }
+            fechaReserva: this.fechaReserva
         }
 
-        this.reservas.push(reserva);
+        // this.reservas.push(reserva);
+        await fetch(apiUrl, {
+          mode: 'no-cors',
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(reserva)
+        });
+
+        await this.fetchReservas();
 
         this.id = ""
         this.nombre = ""
         this.correo = ""
         this.laboratorio = ""
-        this.fecha = this.getCurrentDateTime();
+        this.fechaReserva = this.getCurrentDateTime();
         this.$refs.Close.click();
       },
-      removeReserva(id) {
-        this.reservas = this.reservas.filter(reserva => reserva.id !== id)
+      async removeReserva(idReserva) {
+        const reserva = this.reservas.find(reserva => reserva.idReserva === idReserva);
+
+        await fetch(apiUrl, {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(reserva)
+        });
+
+        await this.fetchReservas();
+      },
+      async editReserva() {
+        let reserva = {
+          idReserva: this.idReserva,
+          id: this.id,
+          nombre: this.nombre,
+          correo: this.correo,
+          laboratorio: this.laboratorio,
+          fechaReserva: this.fechaReserva
+        };
+
+        // this.reservas.push(reserva);
+        await fetch(apiUrl, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(reserva)
+        });
+
+        await this.fetchReservas();
+
+        this.id = ""
+        this.nombre = ""
+        this.correo = ""
+        this.laboratorio = ""
+        this.fechaReserva = this.getCurrentDateTime();
+        this.$refs.closeEdit.click();
       },
       openEditModal(reserva) {
-        // Populate the form fields with the selected reservation data
+
+        this.idReserva = reserva.idReserva;
         this.id = reserva.id;
         this.nombre = reserva.nombre;
-        this.correo = reserva.correo; // Assuming 'correo' is the desired email field
+        this.correo = reserva.correo;
         this.laboratorio = reserva.laboratorio;
-        this.fecha = reserva.fecha;
+        this.fechaReserva = reserva.fechaReserva;
 
-        this.$refs.createModal.click();
-
-        // this.id = ""
-        // this.nombre = ""
-        // this.correo = ""
-        // this.laboratorio = ""
-        // this.fecha = this.getCurrentDateTime();
-        // this.$refs.Close.click();
       },
       getCurrentDateTime() {
         const now = new Date();
         const offset = now.getTimezoneOffset();
         return new Date(now.getTime() - offset * 60 * 1000).toISOString().slice(0, 16);
       },
-      async getReservas() {
-        const response = await fetch('http://localhost:3000/reservas')
-        this.reservas = await response.json()
-      },
       mostrarReservasPasadas() {
         this.mostrarPasadas = !this.mostrarPasadas;
-      }
+      },
+      formatDate(fechaReserva) {
+        const date = new Date(fechaReserva);
+        const formattedDate = date.toLocaleString('es-DO', {
+          day: '2-digit',
+          month: '2-digit',
+          year: 'numeric',
+          hour: '2-digit',
+          minute: '2-digit'
+        });
+        return formattedDate;
+      },
     },
 }
 </script>
